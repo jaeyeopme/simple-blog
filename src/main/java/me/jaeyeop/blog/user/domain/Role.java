b5fb1715f1f1e6b784a3ca8dc0421d3e0d145f40
jaeyeopme <jaeyeopme@gmail.com>
2022-09-27 22:34:16 +0900

    feat: User 도메인 추가

---
+++ src/main/java/me/jaeyeop/blog/user/domain/Role.java
@@ -0,0 +1,10 @@
+package me.jaeyeop.blog.user.domain;
+
+import lombok.Getter;
+
+@Getter
+public enum Role {
+
+  USER, ADMIN
+
+}
---
+++ src/main/java/me/jaeyeop/blog/user/domain/User.java
@@ -0,0 +1,40 @@
+package me.jaeyeop.blog.user.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder(access = AccessLevel.PACKAGE)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column
+  private String picture;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false)
+  private Role role;
+
+}
